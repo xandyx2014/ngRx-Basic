494d31ec7bfc8f27fae74cf4fbcd5a069fe1b763
xandyx2014 <xandyx2014@gmail.com>
2018-08-11 15:37:16 -0400

    comensando parte front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http';
+// route
+import { AppRoutingModule } from './app-routing.module';
+// module custom
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    UsuariosModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: []
+})
+export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
+  constructor( public usuarioService: UsuarioService ) { }
+
+  ngOnInit() {
+    this.usuarioService.getUsers().subscribe( data => {
+      console.log(data);
+      this.usuarios = data;
+    });
+  }
+
+}
